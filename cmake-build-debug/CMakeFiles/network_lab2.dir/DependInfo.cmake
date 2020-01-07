15c2d56bba491ea6242c9f800356d64c4340ca52
copyright <barcelonakeyfc@qq.com>
2020-01-07 18:53:35 +0800

    final

---
+++ cmake-build-debug/CMakeFiles/network_lab2.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/copyright/CLionProjects/network_lab2/GBN/GoBackN_RdtReceiver.cpp" "/home/copyright/CLionProjects/network_lab2/cmake-build-debug/CMakeFiles/network_lab2.dir/GBN/GoBackN_RdtReceiver.cpp.o"
+  "/home/copyright/CLionProjects/network_lab2/GBN/GoBackN_RdtSender.cpp" "/home/copyright/CLionProjects/network_lab2/cmake-build-debug/CMakeFiles/network_lab2.dir/GBN/GoBackN_RdtSender.cpp.o"
+  "/home/copyright/CLionProjects/network_lab2/SR/SelectRdtReceiver.cpp" "/home/copyright/CLionProjects/network_lab2/cmake-build-debug/CMakeFiles/network_lab2.dir/SR/SelectRdtReceiver.cpp.o"
+  "/home/copyright/CLionProjects/network_lab2/SR/SelectRdtSender.cpp" "/home/copyright/CLionProjects/network_lab2/cmake-build-debug/CMakeFiles/network_lab2.dir/SR/SelectRdtSender.cpp.o"
+  "/home/copyright/CLionProjects/network_lab2/TCP/TcpRdtReceiver.cpp" "/home/copyright/CLionProjects/network_lab2/cmake-build-debug/CMakeFiles/network_lab2.dir/TCP/TcpRdtReceiver.cpp.o"
+  "/home/copyright/CLionProjects/network_lab2/TCP/TcpRdtSender.cpp" "/home/copyright/CLionProjects/network_lab2/cmake-build-debug/CMakeFiles/network_lab2.dir/TCP/TcpRdtSender.cpp.o"
+  "/home/copyright/CLionProjects/network_lab2/Wait/StopWait.cpp" "/home/copyright/CLionProjects/network_lab2/cmake-build-debug/CMakeFiles/network_lab2.dir/Wait/StopWait.cpp.o"
+  "/home/copyright/CLionProjects/network_lab2/Wait/StopWaitRdtReceiver.cpp" "/home/copyright/CLionProjects/network_lab2/cmake-build-debug/CMakeFiles/network_lab2.dir/Wait/StopWaitRdtReceiver.cpp.o"
+  "/home/copyright/CLionProjects/network_lab2/Wait/StopWaitRdtSender.cpp" "/home/copyright/CLionProjects/network_lab2/cmake-build-debug/CMakeFiles/network_lab2.dir/Wait/StopWaitRdtSender.cpp.o"
+  "/home/copyright/CLionProjects/network_lab2/main.cpp" "/home/copyright/CLionProjects/network_lab2/cmake-build-debug/CMakeFiles/network_lab2.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././header"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/network_lab2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/network_lab2.dir/GBN/GoBackN_RdtReceiver.cpp.o"
+  "CMakeFiles/network_lab2.dir/GBN/GoBackN_RdtSender.cpp.o"
+  "CMakeFiles/network_lab2.dir/SR/SelectRdtReceiver.cpp.o"
+  "CMakeFiles/network_lab2.dir/SR/SelectRdtSender.cpp.o"
+  "CMakeFiles/network_lab2.dir/TCP/TcpRdtReceiver.cpp.o"
+  "CMakeFiles/network_lab2.dir/TCP/TcpRdtSender.cpp.o"
+  "CMakeFiles/network_lab2.dir/Wait/StopWait.cpp.o"
+  "CMakeFiles/network_lab2.dir/Wait/StopWaitRdtReceiver.cpp.o"
+  "CMakeFiles/network_lab2.dir/Wait/StopWaitRdtSender.cpp.o"
+  "CMakeFiles/network_lab2.dir/main.cpp.o"
+  "network_lab2"
+  "network_lab2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/network_lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
